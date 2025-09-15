8ec865161e31fec55d62137bd7d356416499e711
Rajib Mondal <rajib.3t@gmail.com>
2025-09-15 23:03:11 +0530

    update user add function

--- src/app/pages/main/tenant/users/user-create/user-create.ts
+++ src/app/pages/main/tenant/users/user-create/user-create.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule,User as UserIcon , Mail as MailIcon, LockKeyhole as LockKeyholeIcon} from 'lucide-angular';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { timer } from 'rxjs';
 @Component({
   selector: 'app-user-create',
-  imports: [],
+  imports: [CommonModule, LucideAngularModule, ReactiveFormsModule],
   templateUrl: './user-create.html',
   styleUrl: './user-create.css'
 })
 export class UserCreate {
-
+  readonly UserIcon = UserIcon;
+  readonly MailIcon = MailIcon;
+  readonly LockKeyholeIcon = LockKeyholeIcon;
+  errorMessage: string | null = null;
+  successMessage: string | null = null;
+  isSubmitting = false;
 }
